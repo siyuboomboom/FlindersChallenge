46b1e34028f0208d00a11a451f5cf07dff9fc17c
siyuboomboom <siyu.tan.1012@hotmail.com>
2017-11-21 21:56:23 +1030

    update for 4 routes

--- src/morialtaRouteScreen.js
+++ src/morialtaRouteScreen.js
@@ -19,97 +19,72 @@ import Geofence from 'react-native-expo-geofence';
 import { Constants, Permissions, Notifications } from 'expo';
 import timer from 'react-native-timer';
 
-const morialtaCoor = require( '../routes/route1.js');
-const triggerPoint1 = require( '../triggerPoints/triggerPoint1.js');
-const centerPoints = triggerPoint1.Points;
+const Coor = require( '../routes/route1.js');
+const polylines = Coor.coor.polylines;
+const triggerPoint = require( '../triggerPoints/triggerPoint1.js');
+const centerPoints = triggerPoint.Points;
 const styles = require('../styles.js');
+const parameters = require('../triggerPoints/parameters.js');
+
+const radius = parameters.radius;
+const locatingInterval = parameters.locatingInterval;
+const alertInterval = parameters.alertInterval;
 
 class morialtaRouteScreen extends Component {
 
   static navigationOptions = {
-    title: 'MorialtaRoute',
+    title: 'Morialta Route',
   };
-
-  currentPoint = { latitude : -34.89680   , longitude : 138.69200  };
 
   constructor(props) {
     super(props);
+    this.region = {
+      latitude: polylines[0].coordinates[400].latitude,
+      longitude: polylines[0].coordinates[400].longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    };
+    this.markers = [{
+      latlng:{latitude: -34.928534,
+      longitude: 138.599854,},
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+      title:'you are here',
+    }];
     this.state = {
-      region: {
-        latitude: -34.928534,
-        longitude: 138.599854,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      },
-      markers:[{
-        latlng:{latitude: -34.928534,
-        longitude: 138.599854,},
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-        title:'you are here',
-        description:'you are here',
-      }],
-      distance: 200,  //Siyu: 200 meters
       error: null,
       isStarted: false,
-    }
+    };
   }
-  /**
-   * Ref: https://facebook.github.io/react-native/docs/geolocation.html
-   */
 
-  onRegionChange(region) {
-    this.setState({ region });
+  componentDidMount() {
+    timer.setInterval("intervalID", this.updatePosition.bind(this), locatingInterval);
   }
-  
-  async componentDidMount() {
-    let result = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-    if (Constants.lisDevice && result.status === 'granted') {
-     console.log('Notification permissions granted.')
-    } //ask for permissions to send local notification
 
+  updatePosition() {
     this.watchId = navigator.geolocation.watchPosition(
       (data) => {
-        this.setState({
-          region: {
-            latitude: data.coords.latitude,
-            longitude: data.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          },
-          markers:[{
-            latlng:{latitude: data.coords.latitude,
-            longitude: data.coords.longitude,},
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-            title:'you are here',
-            description:'you are here',
-          }],
-          error: null,
-        });
+        this.markers = [{
+          latlng:{latitude: data.coords.latitude,
+          longitude: data.coords.longitude,},
+          title:'you are here',
+        }];
+        this.setState({error: null});
       },
       (error) => this.setState({ error: error.message }),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
     );
-
   }
-/*
-  componentWillUnmount() {
-    navigator.geolocation.clearWatch(this.watchId);
-  }*/
-
-
 
   handleClick() {
     if(this.state.isStarted) {
       timer.clearInterval("timerInterval");
-      this.stopJourney();
       this.setState({
         isStarted: false
       });
     }
     else {
-      timer.setInterval("timerInterval", this.startJourney.bind(this), 30000); //update per 10s
+      timer.setInterval("timerInterval", this.startJourney.bind(this), alertInterval); //millisenconds
       this.setState({
         isStarted: true,
       });
@@ -117,45 +92,21 @@ class morialtaRouteScreen extends Component {
   }
 
   startJourney() {
-
-    const result = [];
     const distance = [];
 
     for (i = 0; i < centerPoints.length; i++) {
-      result[i] = Geofence.filterByProximity(centerPoints[i], this.state.markers[0].latlng, this.state.distance/1000);
-      distance[i] = Geofence.distanceInKM(centerPoints[i], this.state.markers[0].latlng);
+      distance[i] = Geofence.distanceInKM(centerPoints[i], this.markers[0].latlng);
     }
 
     for (i = 0; i < centerPoints.length; i++) {
-
-      if(result[i][0]) {
-
-        const localNotification = {
-          title: 'Fence ' + (i + 1),
-          body: centerPoints[i].notification,
-          ios: {
-            sound: true
-          },
-          android: {
-            sound: true,
-            priority: 'high',
-            sticky: false,
-            vibrate: true
-          }
-        };
-
-        Notifications.presentLocalNotificationAsync(localNotification);
-        Alert.alert(centerPoints[i].notification );
-      }
-      else {
-        //Alert.alert("You are not in " + centerPoints[i].notification);
-      }
-    }
-    
+      if(distance[i] <= radius/1000) {
+        Alert.alert("Fence" + (i + 1), centerPoints[i].notification );
+      } 
+    }   
   }
 
-  stopJourney() {
-
+  onRegionChange(region) {
+    this.region = region;
   }
 
   render() {
@@ -167,34 +118,33 @@ class morialtaRouteScreen extends Component {
         />
         <MapView
           style={styles.map}
-          region={this.state.region}
+          region={this.region}
           onRegionChange={(region) => this.onRegionChange(region)}
         >
-        {this.state.markers.map(marker => (
-          <MapView.Marker
-          coordinate={marker.latlng}
-          title={marker.title}
-          description={marker.description}
-          />
-        ))}
-        {morialtaCoor.coor.polylines.map(polyline => (
-          <MapView.Polyline
-            key={polyline.id}
-            coordinates={polyline.coordinates}
-            strokeColor="blue"
-            fillColor="red"
-            strokeWidth={2}
-          />
-        ))}
-
-        {triggerPoint1.Points.map( x => (
-          <MapView.Circle
-            center={x}
-            radius={ this.state.distance }
-            strokeColor='transparent'
-            fillColor="rgba(0, 0, 0, 0.2)"
-          />
-        ))}
+          {this.markers.map(marker => (
+            <MapView.Marker
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+            />
+          ))}
+          {polylines.map(polyline => (
+            <MapView.Polyline
+              key={polyline.id}
+              coordinates={polyline.coordinates}
+              strokeColor="blue"
+              fillColor="red"
+              strokeWidth={2}
+            />
+          ))}
+          {centerPoints.map( x => (
+            <MapView.Circle
+              center={x}
+              radius={ radius }
+              strokeColor='transparent'
+              fillColor="rgba(0, 0, 0, 0.2)"
+            />
+          ))}
         </MapView>
       </View>
     );
--- src/mylorRouteScreen.js
+++ src/mylorRouteScreen.js
@@ -1,4 +1,4 @@
-//class for Mylor Route
+//class for Route Mylor
 import React, { Component } from 'react';
 import {
   StackNavigator,
@@ -14,116 +14,137 @@ import {
   Alert
 } from 'react-native';
 import MapView from 'react-native-maps'
-import DeepLinking from 'react-native-deep-linking';
 import ActionButton from '../components/ActionButton';
 import Geofence from 'react-native-expo-geofence';
 import { Constants, Permissions, Notifications } from 'expo';
-import { Button } from 'react-native-elements';
-import { Icons } from 'react-native-elements';
+import timer from 'react-native-timer';
 
-const mylorCoor = require( '../routes/route3.js');
+const Coor = require( '../routes/route3.js');
+const polylines = Coor.coor.polylines;
+const triggerPoint = require( '../triggerPoints/triggerPoint3.js');
+const centerPoints = triggerPoint.Points;
 const styles = require('../styles.js');
+const parameters = require('../triggerPoints/parameters.js');
+
+const radius = parameters.radius;
+const locatingInterval = parameters.locatingInterval;
+const alertInterval = parameters.alertInterval;
 
 class mylorRouteScreen extends Component {
+
   static navigationOptions = {
-    title: 'MylorRoute',
+    title: 'Mylor Route',
   };
 
   constructor(props) {
     super(props);
-
+    this.region = {
+      latitude: polylines[0].coordinates[400].latitude,
+      longitude: polylines[0].coordinates[400].longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    };
+    this.markers = [{
+      latlng:{latitude: -34.928534,
+      longitude: 138.599854,},
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+      title:'you are here',
+    }];
     this.state = {
-      region: {
-        latitude: -34.928534,
-        longitude: 138.599854,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      },
-      markers:[{
-        latlng:{latitude: -34.928534,
-        longitude: 138.599854,},
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-        title:'you are here',
-        description:'you are here',
-      }],
-
+      error: null,
+      isStarted: false,
+    };
   }
-}
-  /**
-   * Ref: https://facebook.github.io/react-native/docs/geolocation.html
-   */
-  getAndUpdateLocation() {
-    navigator.geolocation.getCurrentPosition(
-      (data) => {
-        const region = {
-          latitude: data.coords.latitude,
-          longitude: data.coords.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        };
-        const marker = [{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-          title:'you are here',
-          description:'you are here',
-        }];
-        this.setState({
-          region: {
-            latitude: data.coords.latitude,
-            longitude: data.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          },
-          markers:[{
-            latlng:{latitude: data.coords.latitude,
-            longitude: data.coords.longitude,},
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-            title:'you are here',
-            description:'you are here',
-          }],
 
-        });
+  componentDidMount() {
+    timer.setInterval("intervalID", this.updatePosition.bind(this), locatingInterval);
+  }
+
+  updatePosition() {
+    this.watchId = navigator.geolocation.watchPosition(
+      (data) => {
+        this.markers = [{
+          latlng:{latitude: data.coords.latitude,
+          longitude: data.coords.longitude,},
+          title:'you are here',
+        }];
+        this.setState({error: null});
       },
-      (err) => {
-        console.log('err', err);
-      },
-      {}
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
     );
   }
 
+  handleClick() {
+    if(this.state.isStarted) {
+      timer.clearInterval("timerInterval");
+      this.setState({
+        isStarted: false
+      });
+    }
+    else {
+      timer.setInterval("timerInterval", this.startJourney.bind(this), 30000); //millisenconds
+      this.setState({
+        isStarted: true,
+      });
+    }
+  }
+
+  startJourney() {
+    const distance = [];
+
+    for (i = 0; i < centerPoints.length; i++) {
+      distance[i] = Geofence.distanceInKM(centerPoints[i], this.markers[0].latlng);
+    }
+
+    for (i = 0; i < centerPoints.length; i++) {
+      if(distance[i] <= radius/1000) {
+        Alert.alert("Fence" + (i + 1), centerPoints[i].notification );
+      } 
+    }    
+  }
+
   onRegionChange(region) {
-    this.setState({ region });
+    this.region = region;
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <ActionButton title="Find me!" onPress={() => this.getAndUpdateLocation()} />
+        <ActionButton 
+          title={ this.state.isStarted ? "Stop" : "Start" }
+          onPress={ this.handleClick.bind(this) } 
+        />
         <MapView
           style={styles.map}
-          region={this.state.region}
+          region={this.region}
           onRegionChange={(region) => this.onRegionChange(region)}
-          >
-         {this.state.markers.map(marker => (
-         <MapView.Marker
-         coordinate={marker.latlng}
-         title={marker.title}
-         description={marker.description}
-         />
-       ))}
-        {mylorCoor.coor.polylines.map(polyline => (
-        <MapView.Polyline
-          key={polyline.id}
-          coordinates={polyline.coordinates}
-          strokeColor="blue"
-          fillColor="red"
-          strokeWidth={2}
-          />
-        ))}
+        >
+          {this.markers.map(marker => (
+            <MapView.Marker
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+            />
+          ))}
+          {polylines.map(polyline => (
+            <MapView.Polyline
+              key={polyline.id}
+              coordinates={polyline.coordinates}
+              strokeColor="blue"
+              fillColor="red"
+              strokeWidth={2}
+            />
+          ))}
+          {centerPoints.map( x => (
+            <MapView.Circle
+              center={x}
+              radius={ radius }
+              strokeColor='transparent'
+              fillColor="rgba(0, 0, 0, 0.2)"
+            />
+          ))}
         </MapView>
       </View>
     );
